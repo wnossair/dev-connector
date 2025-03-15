70dc2b2bb7c61c3f53ecba6fcf06df17e7afc68f
wnossair <wassim.nossair@gmail.com>
2025-03-14 21:05:06 -0400

    M3V2: Add User model + implement registration POST

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const posts = require('./routes/api/posts.js');
 
 const app = express();
 
+// Body parser middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 // DB Config
 const db = require('./config/keys').mongoURI;
 
