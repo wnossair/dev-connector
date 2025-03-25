de989afeb4fb883d74b6b22f484ca82a8d7d70cb
wnossair <wassim.nossair@gmail.com>
2025-03-25 14:36:40 -0400

    M5V3: Get all posts + Get Post by ID + Delete Post by ID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -12,6 +12,40 @@ const validatePostInput = require("../../validation/post");
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res
+        .status(404)
+        .json({ post_error: `${err}: No posts found with that id.` })
+    );
+});
+
+// @route   GET api/posts/:id
+// @desc    Get all posts
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      if (!post) {
+        return res.status(404).json({
+          post_error: `${req.params.id}: No post found with that id.`,
+        });
+      }
+      res.json(post);
+    })
+    .catch(err =>
+      res
+        .status(404)
+        .json({ post_error: `${err}: No post found with that id.` })
+    );
+});
+
 // @route   POST api/posts
 // @desc    Create posts
 // @access  Private
@@ -33,6 +67,34 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post by ID
+// @access  Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findOne({ _id: req.params.id })
+      .populate("user", "_id")
+      .then(post => {
+        if (post.user.id !== req.user.id) {
+          return res
+            .status(401)
+            .json({ user_error: "Operation forbidden: Not post owner" });
+        }
+
+        Post.deleteOne({ _id: req.params.id })
+          .then(result => {
+            if (result.acknowledged) {
+              return res.json({ sucess: true });
+            }
+          })
+          .catch(err => res.status(404).json(err));
+      })
+      .catch(err => res.status(404).json(err));
   }
 );
 
