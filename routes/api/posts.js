1c5ea9a8a6551b3b62849f47ea3bc67cf7b89887
wnossair <wassim.nossair@gmail.com>
2025-03-25 09:57:05 -0400

    M5V2: Added Post validation + new post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Post Specific imports
+const Post = require("../../models/Post");
+const validatePostInput = require("../../validation/post");
 
 // @route   GET api/posts/test
 // @desc    Tests posts route
 // @access  Public
-router.get('/test', (req, res) => res.json({msg: "Posts Works"}));
+router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+// @route   POST api/posts
+// @desc    Create posts
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id,
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
