7fa4025cbf6c88c6e15c28711ca8e86843753eac
wnossair <wassim.nossair@gmail.com>
2025-03-28 10:41:05 -0400

    M6V4: Added Navbar + Landing + Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>My React App</h1>
+      <Navbar />
+      <Landing />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
