53c3eed4c2c3c85bcbd8a0943891037207c866fb
wnossair <wassim.nossair@gmail.com>
2025-04-15 19:51:52 -0400

    M11V02: Profile Items

---
+++ client/src/components/developers/ProfileItem.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ProfileItem = ({ profile }) => {
+  const { avatar, name, _id } = profile.user;
+  const { status, company, location, skills } = profile;
+
+  return (
+    <div className="card card-body bg-light mb-3" key={_id}>
+      <div className="row">
+        <div className="col-2">
+          <img className="rounded-circle" src={avatar} alt={name} />
+        </div>
+        <div className="col-lg-6 col-md-4 col-8">
+          <h3>{name}</h3>
+          <p>
+            {status} at {company ?? ""}
+          </p>
+          <p>{location ?? ""}</p>
+          <Link to={`/profile/user/${_id}`} className="btn btn-info">
+            View Profile
+          </Link>
+        </div>
+        <div className="col-md-4 d-none d-lg-block">
+          <h4>Skill Set</h4>
+          <ul className="list-group">
+            {Array.isArray(skills) && skills.slice(0, 4).map((skill, index) => (
+              <li className="list-group-item" key={`skill-${index}`}>
+                <span className="bi bi-check-circle-fill text-success me-2"></span>
+                {skill}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileItem;
--- client/src/components/developers/Profiles.jsx
+++ client/src/components/developers/Profiles.jsx
@@ -3,6 +3,7 @@ import { loadAllProfiles } from "../../features/profile/profileSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { Spinner } from "../common/Feedback";
 import { Link } from "react-router-dom";
+import ProfileItem from "./ProfileItem";
 
 const Profiles = () => {
   const dispatch = useDispatch();
@@ -15,101 +16,27 @@ const Profiles = () => {
   }, []);
 
   useEffect(() => {
-    if (profiles) {
+    if (allProfiles) {
       setProfiles(allProfiles);
     }
   }, [allProfiles]);
 
   return (
     <div>
-      {!profiles || loading ? (
+      {loading ? (
         <Spinner />
-      ) : profiles.length > 0 ? (
+      ) : profiles?.length > 0 ? (
         // Profiles
-        <div class="profiles">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-12">
-                <h1 class="display-4 text-center">Developer Profiles</h1>
-                <p class="lead text-center">Browse and connect with developers</p>
+        <div className="profiles">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 className="display-4 text-center">Developer Profiles</h1>
+                <p className="lead text-center">Browse and connect with developers</p>
 
-                {/* TODO: Profile Item */}
-                <div class="card card-body bg-light mb-3">
-                  <div class="row">
-                    <div class="col-2">
-                      <img
-                        class="rounded-circle"
-                        src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
-                        alt=""
-                      />
-                    </div>
-                    <div class="col-lg-6 col-md-4 col-8">
-                      <h3>John Doe</h3>
-                      <p>Developer at Microsoft</p>
-                      <p>Seattle, WA</p>
-                      <a href="profile.html" class="btn btn-info">
-                        View Profile
-                      </a>
-                    </div>
-                    <div class="col-md-4 d-none d-lg-block">
-                      <h4>Skill Set</h4>
-                      <ul class="list-group">
-                        <li class="list-group-item">
-                          <i class="fa fa-check pr-1"></i>HTML
-                        </li>
-                        <li class="list-group-item">
-                          <i class="fa fa-check pr-1"></i>CSS
-                        </li>
-                        <li class="list-group-item">
-                          <i class="fa fa-check pr-1"></i>JavaScript
-                        </li>
-                        <li class="list-group-item">
-                          <i class="fa fa-check pr-1"></i>Python
-                        </li>
-                        <li class="list-group-item">
-                          <i class="fa fa-check pr-1"></i>C#
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card card-body bg-light mb-3">
-                  <div class="row">
-                    <div class="col-2">
-                      <img
-                        class="rounded-circle"
-                        src="https://www.gravatar.com/avatar/anything?s=200&d=mm"
-                        alt=""
-                      />
-                    </div>
-                    <div class="col-lg-6 col-md-4 col-8">
-                      <h3>John Doe</h3>
-                      <p>Developer at Microsoft</p>
-                      <p>Seattle, WA</p>
-                      <a href="profile.html" class="btn btn-info">
-                        View Profile
-                      </a>
-                    </div>
-                    <div class="col-md-4 d-none d-lg-block">
-                      <h4>Skill Set</h4>
-                      <ul class="list-group">
-                        <li class="list-group-item">
-                          <i class="fa fa-check pr-1"></i>HTML
-                        </li>
-                        <li class="list-group-item">
-                          <i class="fa fa-check pr-1"></i>CSS
-                        </li>
-                        <li class="list-group-item">
-                          <i class="fa fa-check pr-1"></i>PHP
-                        </li>
-                        <li class="list-group-item">
-                          <i class="fa fa-check pr-1"></i>MySQL
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
+                {profiles.map(profile => (
+                  <ProfileItem key={profile._id} profile={profile} />
+                ))}
               </div>
             </div>
           </div>
