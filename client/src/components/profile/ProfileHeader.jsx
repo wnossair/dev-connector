448eae25dd89fa44ddd4679d57a8f7f1f486f8a5
wnossair <wassim.nossair@gmail.com>
2025-09-02 11:38:13 -0400

    HTML error message and formatting

--- client/src/components/profile/ProfileHeader.jsx
+++ client/src/components/profile/ProfileHeader.jsx
@@ -30,68 +30,70 @@ const ProfileHeader = ({ profile }) => {
             <h1 className="display-4 text-center">{name}</h1>
             <p className="lead text-center">
               {role}
-              {company && ` at ${company}`} in
-              {location && <p>{location}</p>}
-              {website && (
-                <a
-                  className="text-white p-2"
-                  href={website}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="bi bi-globe fs-3"></i>
-                </a>
-              )}
-              {social.twitter && (
-                <a
-                  className="text-white p-2"
-                  href={social.twitter}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="bi bi-twitter fs-3"></i>
-                </a>
-              )}
-              {social.facebook && (
-                <a
-                  className="text-white p-2"
-                  href={social.facebook}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="bi bi-facebook fs-3"></i>
-                </a>
-              )}
-              {social.linkedin && (
-                <a
-                  className="text-white p-2"
-                  href={social.linkedin}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="bi bi-linkedin fs-3"></i>
-                </a>
-              )}
-              {social.instagram && (
-                <a
-                  className="text-white p-2"
-                  href={social.instagram}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="bi bi-instagram fs-3"></i>
-                </a>
-              )}
-              {social.youtube && (
-                <a
-                  className="text-white p-2"
-                  href={social.youtube}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="bi bi-youtube fs-3"></i>
-                </a>
-              )}
+              {company && ` at ${company}`}
+              {location && ` in ${location}`}
+              <span className="d-block">
+                {website && (
+                  <a
+                    className="text-white p-2"
+                    href={website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="bi bi-globe fs-3"></i>
+                  </a>
+                )}
+                {social.twitter && (
+                  <a
+                    className="text-white p-2"
+                    href={social.twitter}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="bi bi-twitter fs-3"></i>
+                  </a>
+                )}
+                {social.facebook && (
+                  <a
+                    className="text-white p-2"
+                    href={social.facebook}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="bi bi-facebook fs-3"></i>
+                  </a>
+                )}
+                {social.linkedin && (
+                  <a
+                    className="text-white p-2"
+                    href={social.linkedin}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="bi bi-linkedin fs-3"></i>
+                  </a>
+                )}
+                {social.instagram && (
+                  <a
+                    className="text-white p-2"
+                    href={social.instagram}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="bi bi-instagram fs-3"></i>
+                  </a>
+                )}
+                {social.youtube && (
+                  <a
+                    className="text-white p-2"
+                    href={social.youtube}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="bi bi-youtube fs-3"></i>
+                  </a>
+                )}
+              </span>
             </p>
           </div>
         </div>
